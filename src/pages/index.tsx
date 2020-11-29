ad3f92129eae336217c3aebff5dacd76f8947763
8zca <excref@gmail.com>
2020-11-29 11:09:22 +0900

    リファラtest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Layout } from '@/components/helpers'
 import { Top } from '@/components/standalones'
 
 const IndexPage: React.FC = () => {
+  useEffect(() => {
+    if (document) {
+      console.log('ref', document.referrer)
+    }
+  }, [])
+
   return (
     <Layout title='Home - Dashboard'>
       <Top />
